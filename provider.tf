1164f5d9511d22537d1fc37bcd039b580ac7b2ae
erpranavjoshi <erpranavj@gmail.com>
2020-05-21 17:30:24 +0530

    removed keys

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = var.region
-  secret_key = "9GOmFauRaBRlR8XwrouKwFeOr3gfu+8e3UxG93lC"
-  access_key = "AKIAV5G7ALK5J6DWI43R"
+  secret_key = var.secret_key
+  access_key = var.access_key
 
 }
